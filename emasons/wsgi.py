1dcb68be6a556ff7b6352b7ac2e3734bd847a6b7
Albert Ashaba Aheebwa <trippleauit@hotmail.com>
2024-03-06 15:28:56 +0300

    add tailwind template

--- emasons/wsgi.py
+++ emasons/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for emason_mkt project.
+WSGI config for emasons project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emason_mkt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emasons.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emason_mkt.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emasons.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
