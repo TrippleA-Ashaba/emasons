ef77acea25ac1d3d44e33110ed1d5844cfcde226
Albert Ashaba Aheebwa <trippleauit@hotmail.com>
2024-03-13 22:44:20 +0300

    create user model and permissions

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from auditlog.registry import auditlog
+from author.decorators import with_author
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+
+@with_author
+class User(AbstractUser, TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+
+auditlog.register(User)
